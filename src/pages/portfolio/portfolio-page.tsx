009c2596f90e7ca235b7878a7d53d26e565be4fb
Rubén Triviño Juárez <ruben@willimbu.com>
2020-05-10 19:27:20 +0200

    Update project .json mapper and minor styles

--- src/pages/portfolio/portfolio-page.tsx
+++ src/pages/portfolio/portfolio-page.tsx
@@ -1,23 +1,28 @@
 
 import React, { useEffect, useState} from 'react';
+import './portfolio-page.scss';
 
 function PortfolioPage(props: any){
     const [repos, setRepos] = useState([]);
 
     useEffect(()=>{
-        props.githubApi.getRepost().subscribe((res: any)=>{
+        props.githubApi.getRepos().subscribe((res: any)=>{
             setRepos(res)
         });
     }, [])
 
     return (
         <div className="page">
-            <p>Portfolio Page</p>
             {repos.map((repository: any, index)=>{
                 return (
-                    <div key={index}>
-                        <p>{repository.name}</p>
-                        <p>{repository.html_url}</p>
+                    <div className="project-card" key={index}>
+                        <h1>{repository.project_name}</h1>
+                        <p>{repository.description}</p>
+                        {
+                            repository.technologies.map((technology:string, techIndex: number)=>{
+                                return <span key={techIndex}>#{technology}</span>
+                            })
+                        }
                         <br></br>
                         <br></br>
                     </div>
--- src/services/github-api.ts
+++ src/services/github-api.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/internal/Observable';
 import { forkJoin } from 'rxjs';
 
 export const GithubApi = {
-    getRepost: () => {
+    getRepos: () => {
         return fromFetch("https://api.github.com/users/anexon/repos").pipe(
             mergeMap((res)=>{
                 return res.json();
@@ -12,24 +12,16 @@ export const GithubApi = {
             mergeMap((respos: any[])=>{
                 let reposAbstract: Observable<any>[] = [];
                 respos.forEach(repo=>{
-                    reposAbstract.push(fromFetch(`https://raw.githubusercontent.com/${repo.full_name}/abstract.json`))
+                    reposAbstract.push(fromFetch(`https://raw.githubusercontent.com/${repo.full_name}/master/abstract.json`))
                 })
                 return forkJoin(reposAbstract);
             }),
             mergeMap((responses: any) => {
                 let validResponseBodies: any[] = [];
-                console.log("The responses: ", responses);
                 responses.forEach((response: any) => {
-                    if(response.status != 400) validResponseBodies.push(response.body.json());
+                    if(response.status == 200) validResponseBodies.push(response.json());
                 })
-                return validResponseBodies;
-            }),
-            map((res)=>{
-                console.log("Formatted responses: ", res);
-                return [{
-                    name: "Prueba",
-                    html_url: "Prueba html"
-                }, ()=>{console.log("No existe")}]
+                return forkJoin(validResponseBodies);
             })
         );
     }
