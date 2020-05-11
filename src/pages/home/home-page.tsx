350d022dde87a1dbded2fe834ac0b6f5862faebf
Rubén Triviño Juárez <ruben@willimbu.com>
2020-05-11 21:08:29 +0200

    Add new content to knowledge area. Add some style. Add loading on portfolio area

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -5,37 +5,39 @@ import KnowledgeAreas from '../../components/knowledge-areas/knowledge-areas';
 const HomePage = () => {
     return (
         <div className="page">
-            <div className="personal-summary">
-                <div className="summary-positioning">
-                    <h1>Summary</h1>
-                    <p>
-                        Soy una persona dinámica y creativa, con gran capacidad 
-                        para trabajar sobre presión y para gestionar situaciones imprevistas. <br/><br/>
+            <div className="summary-section">
+                <div className="personal-summary">
+                    <div className="summary-positioning">
+                        <h1>Summary</h1>
+                        <p>
+                            Soy una persona dinámica y creativa, con gran capacidad 
+                            para trabajar sobre presión y para gestionar situaciones imprevistas. <br/><br/>
 
-                        A la hora de abordar un proyecto no pierdo la visión global y mantengo 
-                        el foco sobre los objetivos principales. 
-                        Para mí, la comunicación con el 
-                        equipo es fundamental para orientar los esfuerzos hacía un mismo fin y 
-                        mantener un ambiente enérgico.
-                        <br></br>
-                        <br></br>
-                        <span>#entrepeneur</span>
-                        <span>#adventurer</span>
-                        <span>#curious</span>
-                        <span>#creative</span>
-                    </p>
-                    <div className="separator"></div><br/>
-                    <div className="social-media-icons">
-                        <a href="https://www.linkedin.com/in/tjuarezruben/" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/linkedin.png"/></a>
-                        <a href="https://medium.com/@benru" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/medium.svg"/></a>
-                        <a href="https://twitter.com/benRuTJ" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/twitter.png"/></a>
-                        <a href="https://github.com/Anexon" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/github.png"/></a>
-                        <br></br>
-                        <small>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></small>
+                            A la hora de abordar un proyecto no pierdo la visión global y mantengo 
+                            el foco sobre los objetivos principales. 
+                            Para mí, la comunicación con el 
+                            equipo es fundamental para orientar los esfuerzos hacía un mismo fin y 
+                            mantener un ambiente enérgico.
+                            <br></br>
+                            <br></br>
+                            <span>#entrepeneur</span>
+                            <span>#adventurer</span>
+                            <span>#curious</span>
+                            <span>#creative</span>
+                        </p>
+                        <div className="separator"></div><br/>
+                        <div className="social-media-icons">
+                            <a href="https://www.linkedin.com/in/tjuarezruben/" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/linkedin.png"/></a>
+                            <a href="https://medium.com/@benru" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/medium.svg"/></a>
+                            <a href="https://twitter.com/benRuTJ" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/twitter.png"/></a>
+                            <a href="https://github.com/Anexon" rel="noopener noreferrer" target="_blank"><img alt="" src="resources/github.png"/></a>
+                            <br></br>
+                            <small>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></small>
+                        </div>
                     </div>
                 </div>
+                <div className="header-background"></div>
             </div>
-            <div className="header-background"></div>
             <KnowledgeAreas/>
         </div>
     );
--- src/pages/portfolio/portfolio-page.tsx
+++ src/pages/portfolio/portfolio-page.tsx
@@ -4,30 +4,44 @@ import './portfolio-page.scss';
 
 function PortfolioPage(props: any){
     const [repos, setRepos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(()=>{
         props.githubApi.getRepos().subscribe((res: any)=>{
             setRepos(res)
+            setIsLoading(false)
         });
     }, [])
 
     return (
         <div className="page">
-            {repos.map((repository: any, index)=>{
-                return (
-                    <div className="project-card" key={index}>
-                        <h1>{repository.project_name}</h1>
-                        <p>{repository.description}</p>
-                        {
-                            repository.technologies.map((technology:string, techIndex: number)=>{
-                                return <span key={techIndex}>#{technology}</span>
-                            })
-                        }
-                        <br></br>
-                        <br></br>
+            { !isLoading && (
+                repos.map((repository: any, index)=>{
+                    return (
+                        <div className="project-card" key={index}>
+                            <h1>{repository.project_name}</h1>
+                            <p>{repository.description}</p>
+                            {
+                                repository.technologies.map((technology:string, techIndex: number)=>{
+                                    return <span key={techIndex}>#{technology}</span>
+                                })
+                            }
+                            <br></br>
+                            <br></br>
+                        </div>
+                    );
+                }))
+            }
+            {
+                isLoading && (
+                <div className="loader">
+                    <p>Cargando Repositorios</p>
+                    <div className="loader-positioning">
+                        <div className="lds-dual-ring"></div>
                     </div>
-                );
-            })}
+                </div>
+                )
+            }
         </div>
     )
 }
