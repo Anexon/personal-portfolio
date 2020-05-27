afa1a4b0c283cbef76952c644af95812206fa943
Rubén Triviño Juárez <ruben@willimbu.com>
2020-05-27 18:16:53 +0200

    enhancement: include more i18n tags

--- src/components/knowledge-areas/knowledge-areas.tsx
+++ src/components/knowledge-areas/knowledge-areas.tsx
@@ -22,62 +22,34 @@ function KnowledgeAreas() {
           </figcaption>
         </figure>
         <div className="section-body">
-          <h1>Organización de Empresas</h1>
-          Como cofundador de varias empresas he sido responsable de la
-          administración, organización y coordenación de los recursos
-          necesarios; personal, recursos económicos, definición de objetivos y
-          seguimiento, dirección de tecnológica y coordinación comercial.
+          <h1>
+            <Text tid="businessAdministrationTitle"></Text>
+          </h1>
+          <Text tid="businessAdministrationDescription"></Text>
           <br></br>
           <br></br>
-          En esta labor he empleado varias metodologías y herramientas:
+          <Text tid="businessAdminIntro"></Text>
           <ul>
-            <li>OKRs</li>
-            Identificación de objetivos y key results generales, definición de
-            OKRs e inicitivas por departamento. La utilización de OKRs permite a
-            todos los departamentos de la empresa alinear esfuerzos y trabajar
-            en el mismo sentido. Además, contribuye a la fácil identificación de
-            la aportación de valor de todos los trabajadores y todas las partes;
-            evaluar resultados y reflexionar sobre las principales dificultades
-            de la empresa.
-            <li>Metodologías Agile y SCRUM</li>
-            La implantación de este tipo de mecánicas organizativas en los
-            distintos departamentos la he realizado en coordinación con los
-            responsables de departamentos utilizando herramientas como Trello,
-            Jira, Asana, entre otras, en función de cada caso. El seguimiento
-            semanal del trabajo permite identificar situaciones de bloqueo entre
-            departamentos y fomentar la intercomunicación para avanzar de forma
-            consistente.
-            <li>KPIs</li>
-            La identificación de los parámetros principales del desempeño de la
-            empresa permite hacer un seguimiento objetivo de las operaciones,
-            despliegue de funcionalidades, satisfacción del cliente, etc..
-            Evitando así valoraciones subjetivas y exponer los resultados a
-            todos los trabajadores, permitiendo que todos tengan una clara
-            expectativa de lo que se espera de ellos. Para la identificación de
-            los KPIs es necesario el trabajo con el consejo de administración,
-            departamento financiero y el resto de departamentos que deben
-            comprometerse a exponer aquellas métricas que son importantes para
-            el éxito de la empresa. La publicación de los KPIs la he realizado
-            en formato digital en paneles de métricas; Datastudio, D3 y paneles
-            de desrrollo propio, reportes mensuales en excel, etc..
-            <li>Coordinación de Proyectos</li>
-            La realización de proyectos financiables es un recurso fundamental
-            que permite el desarrollo y la innovación en empresas. He elaborado
-            proyectos para la solicitud de ENISA, SMI y CDTI, que han permitido
-            a la empresa la obtención de recursos para la realización de
-            proyectos de innovación como; digitalización del sector de trabajo
-            temporal, desarrollo de un algoritmo de recomendación y plan de
-            expansión territorial.
-            <li>Reuniones Generales y One-on-One</li>
-            La coordinación entre departamentos la mantengo a través de
-            reuniones semanales en el que se revisan brevemente los hitos
-            alcanzados y las dificultades identificadas. Además, propicio que
-            cada responsable de departamento establezca a su vez reuniones
-            One-on-One con los componentes de sus equipos para identificar
-            puntos a refozar y conseguir un clima laboral estrecho y saludable.
-            Es importantes identificar las necesidades de los trabajadores en un
-            plano profesional y personal para trabajar en conseguirlos y mejorar
-            la realizació personal de todos.
+            <li>
+              <Text tid="businessItem1Title"></Text>
+            </li>
+            <Text tid="businessItem1Description"></Text>
+            <li>
+              <Text tid="businessItem2Title"></Text>
+            </li>
+            <Text tid="businessItem2Description"></Text>
+            <li>
+              <Text tid="businessItem3Title"></Text>
+            </li>
+            <Text tid="businessItem3Description"></Text>
+            <li>
+              <Text tid="businessItem4Title"></Text>
+            </li>
+            <Text tid="businessItem4Description"></Text>
+            <li>
+              <Text tid="businessItem5Title"></Text>
+            </li>
+            <Text tid="businessItem5Description"></Text>
           </ul>
         </div>
       </div>
--- src/languages/es.js
+++ src/languages/es.js
@@ -2,14 +2,67 @@ const es = {
   "summary": "Resumen",
   "summaryDescription1": "Soy una persona dinámica y creativa, con gran capacidad para trabajar sobre presión y para gestionar situaciones imprevistas.",
   "summaryDescription2": `A la hora de abordar un proyecto no pierdo la visión global y mantengo el foco sobre 
-  los objetivos principales. Para mí, la
-  comunicación con el equipo es fundamental para orientar los
-  esfuerzos hacía un mismo fin y mantener un ambiente enérgico.`,
+    los objetivos principales. Para mí, la
+    comunicación con el equipo es fundamental para orientar los
+    esfuerzos hacía un mismo fin y mantener un ambiente enérgico.`,
   "entrepeneurHastag": "#emprendedor",
   "curiosHastag": "#curioso",
   "creativeHastag": "#creativo",
   "adventurerHastag": "#aventurero",
-  "languageAreas": "Áreas de Conocimiento"
+  "knowledgeAreas": "Áreas de Conocimiento",
+  "businessAdministrationTitle": "Organización de Empresas",
+  "businessAdministrationDescription": `Como cofundador de varias empresas he sido responsable de la
+    administración, organización y coordenación de los recursos
+    necesarios; personal, recursos económicos, definición de objetivos y
+    seguimiento, dirección de tecnológica y coordinación comercial.`,
+  "businessAdminIntro": "En esta labor he empleado varias metodologías y herramientas:",
+  "businessItem1Title": "OKRs",
+  "businessItem1Description": `Identificación de objetivos y key results generales, definición de
+    OKRs e inicitivas por departamento. La utilización de OKRs permite a
+    todos los departamentos de la empresa alinear esfuerzos y trabajar
+    en el mismo sentido. Además, contribuye a la fácil identificación de
+    la aportación de valor de todos los trabajadores y todas las partes;
+    evaluar resultados y reflexionar sobre las principales dificultades
+    de la empresa.`,
+  "businessItem2Title": "Metodologías Agile y SCRUM",
+  "businessItem2Description": `La implantación de este tipo de mecánicas organizativas en los
+    distintos departamentos la he realizado en coordinación con los
+    responsables de departamentos utilizando herramientas como Trello,
+    Jira, Asana, entre otras, en función de cada caso. El seguimiento
+    semanal del trabajo permite identificar situaciones de bloqueo entre
+    departamentos y fomentar la intercomunicación para avanzar de forma
+    consistente.`,
+  "businessItem3Title": "KPIs",
+  "businessItem3Description": `La identificación de los parámetros principales del desempeño de la
+    empresa permite hacer un seguimiento objetivo de las operaciones,
+    despliegue de funcionalidades, satisfacción del cliente, etc..
+    Evitando así valoraciones subjetivas y exponer los resultados a
+    todos los trabajadores, permitiendo que todos tengan una clara
+    expectativa de lo que se espera de ellos. Para la identificación de
+    los KPIs es necesario el trabajo con el consejo de administración,
+    departamento financiero y el resto de departamentos que deben
+    comprometerse a exponer aquellas métricas que son importantes para
+    el éxito de la empresa. La publicación de los KPIs la he realizado
+    en formato digital en paneles de métricas; Datastudio, D3 y paneles
+    de desrrollo propio, reportes mensuales en excel, etc..`,
+  "businessItem4Title": "Coordinación de Proyectos",
+  "businessItem4Description": `La realización de proyectos financiables es un recurso fundamental
+    que permite el desarrollo y la innovación en empresas. He elaborado
+    proyectos para la solicitud de ENISA, SMI y CDTI, que han permitido
+    a la empresa la obtención de recursos para la realización de
+    proyectos de innovación como; digitalización del sector de trabajo
+    temporal, desarrollo de un algoritmo de recomendación y plan de
+    expansión territorial.`,
+  "businessItem5Title": "Reuniones Generales y One-on-One",
+  "businessItem5Description": `La coordinación entre departamentos la mantengo a través de
+    reuniones semanales en el que se revisan brevemente los hitos
+    alcanzados y las dificultades identificadas. Además, propicio que
+    cada responsable de departamento establezca a su vez reuniones
+    One-on-One con los componentes de sus equipos para identificar
+    puntos a refozar y conseguir un clima laboral estrecho y saludable.
+    Es importantes identificar las necesidades de los trabajadores en un
+    plano profesional y personal para trabajar en conseguirlos y mejorar
+    la realizació personal de todos.`
 };
 
 export default es;
